0f37bc67ec1ee02c1c4e15d9c2d64451eb13ec17
fpatane <fpatane@pccube.com>
2020-09-06 19:49:56 +0200

    hide

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
-  db_name : 'db',
-  collection_name : 'quiz',
-  db_uri : "mongodb+srv://francesco:11031992@cluster0.clylk.mongodb.net/db?retryWrites=true&w=majority"
+  db_name : '',
+  collection_name : '',
+  db_uri : ""
 }
 
 module.exports = config;
